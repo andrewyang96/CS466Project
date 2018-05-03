4639a670d6551be6e4bcfadf74ee6f979b3b950a
Andrew Yang <andrewyang96@gmail.com>
2018-05-03 16:45:20 -0500

    Update test script

--- test.py
+++ test.py
@@ -1,22 +1,32 @@
 from algorithms.global_alignment import needleman_wunsch
 from algorithms.local_alignment import smith_waterman
-#from algorithms.Hirschberg import
 
-from Bio import SeqIO
+import pickle
 import sys
 import subprocess
-
-arguments = sys.argv
+import time
 
 sys.setrecursionlimit(25000)
 
 if __name__ == '__main__':
-    seqs = SeqIO.parse('dna.fasta', 'fasta')
-    seq1 = next(seqs)
-    seq2 = next(seqs)
-    seq3 = next(seqs)
     h_commands = ['python', 'algorithms/Hirschberg.py', 'mutatedSeq.txt', '-2', '-1', '2', '>', 'HirschbergResutls.txt']
+    with open('mutatedSeq.txt', 'rb') as f:
+        seqs = pickle.load(f)
 
-    global_result = needleman_wunsch(seq1, seq2)
-    local_result = smith_waterman(seq1, seq3)
+    print('Testing global')
+    start = time.time()
+    global_result = needleman_wunsch(seqs[0], seqs[1])
+    end = time.time()
+    global_time_elapsed = end - start
+
+    print('Testing local')
+    start = time.time()
+    local_result = smith_waterman(seqs[0], seqs[1])
+    end = time.time()
+    local_time_elapsed = end - start
+
+    print('Testing Hirschberg')
+    start = time.time()
     subprocess.call(h_commands)
+    end = time.time()
+    h_time_elapsed = end - start
