d4e33e257792b6610f9d14f3c8df1325e489a64a
Andrew Yang <andrewyang96@gmail.com>
2018-05-03 14:23:42 -0500

    Compare entire seqs in test

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ sys.setrecursionlimit(25000)
 
 if __name__ == '__main__':
     seqs = SeqIO.parse('dna.fasta', 'fasta')
-    seq1 = next(seqs)[:1000]
-    seq2 = next(seqs)[:1000]
-    seq3 = next(seqs)[:100]
+    seq1 = next(seqs)
+    seq2 = next(seqs)
+    seq3 = next(seqs)
 
     global_result = needleman_wunsch(seq1, seq2)
     local_result = smith_waterman(seq1, seq3)
