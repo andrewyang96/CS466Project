341542c4ea4930343f80c72b5a3fc6b29263555f
jhkwon3 <jhkwon3@illinois.edu>
2018-05-03 16:33:13 -0500

    less data + edited test.py

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 from algorithms.global_alignment import needleman_wunsch
 from algorithms.local_alignment import smith_waterman
+#from algorithms.Hirschberg import
 
 from Bio import SeqIO
 import sys
+import subprocess
+
+arguments = sys.argv
+
 sys.setrecursionlimit(25000)
 
 if __name__ == '__main__':
@@ -10,6 +15,8 @@ if __name__ == '__main__':
     seq1 = next(seqs)
     seq2 = next(seqs)
     seq3 = next(seqs)
+    h_commands = ['python', 'algorithms/Hirschberg.py', 'mutatedSeq.txt', '-2', '-1', '2', '>', 'HirschbergResutls.txt']
 
     global_result = needleman_wunsch(seq1, seq2)
     local_result = smith_waterman(seq1, seq3)
+    subprocess.call(h_commands)
